29e7b9c3c3f985cfed7fa8ad5ce1f5861ef3e1a6
maxwell <maxwell@butthole.town>
2022-03-05 15:55:38 -0800

    Add display update logic
    Make it a little more funky

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
-import { scaleLinear } from 'd3';
+import { scaleDivergingPow } from 'd3';
 
 class App extends React.Component {
 
@@ -15,8 +15,8 @@ class App extends React.Component {
   var baseDiv = this.myRef.current;
 
   // Create SVG for clock visualization
-  var width = 460
-  var height = 460
+  var width = window.innerWidth;
+  var height = window.innerHeight;
   var svg = d3.select(baseDiv)
     .append("svg")
     .attr("width", width)
@@ -24,11 +24,12 @@ class App extends React.Component {
 
   // Mock up some time data
   var times = [
-    { name : "work", time : 1200},
-    { name : "sleep", time : 500},
-    { name : "play", time : 800},
+    { name : "work", time : 0},
+    { name : "sleep", time : 50},
+    { name : "play", time : 100},
   ]
-// bubble size function
+
+  // bubble size function
 var bubbleSize = function(bubble) {
   var allTimes = times.map(entry => entry.time);
   var min = Math.min(...allTimes);
@@ -41,7 +42,6 @@ var bubbleSize = function(bubble) {
   return scale(bubble.time);
 }
 
-
 // draw some test bubbles
 var bubbles = svg.append("g")
   .selectAll("circle")
@@ -52,9 +52,9 @@ var bubbles = svg.append("g")
     .attr("r", d => bubbleSize(d))
     .attr("cx", width / 2)
     .attr("cy", height / 2)
-    .style("fill", "lightblue")
+    .style("fill", "lightyellow")
     .style("fill-opacity", 0.8)
-    .attr("stroke", "black")
+    .attr("stroke", "white")
     .style("stroke-width", 1);
 
 // Features of the forces applied to the nodes:
@@ -64,20 +64,42 @@ var simulation = d3.forceSimulation()
     .force("collide", d3.forceCollide().strength(.2).radius(53).iterations(1)) // Force that avoids circle overlapping
 
 
-simulation
-.nodes(times)
-.on("tick", function(d){
-  bubbles
-      .attr("cx", function(d){ return d.x; })
-      .attr("cy", function(d){ return d.y; })
-});
+var updateDisplay = function() {
+
+  // Update sizes of the bubble clocks
+  d3.selectAll("circle")
+  .attr("r", d => bubbleSize(d));
+
+  // Re-run the force simulation
+  simulation
+    .nodes(times)
+    .on("tick", function(d){
+      bubbles
+          .attr("cx", function(d){ return d.x; })
+          .attr("cy", function(d){ return d.y; })
+    });
+}
+
+var timerLoop = async function() {
+  while (true) {
+    var radius = Math.floor(Math.random() * 101)
+    var index = Math.floor(Math.random() * 3)
+
+    updateDisplay();
+    times[index].time = radius;
+    await new Promise(r => setTimeout(r, 1000));
+  }
+}
+
+timerLoop();
 
 }
 
  render() {
-  return (
+  return ([
     <div ref={this.myRef}>
     </div>
+    ]
   );
  }
 }
