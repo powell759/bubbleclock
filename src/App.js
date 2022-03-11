ea2a8f4bef5fc0e04f03d90ed0bc904a6ceeb761
maxwell <maxwell@butthole.town>
2022-03-10 23:55:08 -0800

    Add dynamic color, ability to add bubbles, and more
    Choose color randomly when added
    Click background to add bubbles
    Bug fix: clicking text starts/stops the bubble

--- src/App.js
+++ src/App.js
@@ -11,84 +11,130 @@ class App extends React.Component {
 
     // Global clock data
     this.categories = [
-      { name : "work", runs : [], color: 1},
-      { name : "sleep", runs : [], color: 2},
-      { name : "play", runs : [], color: 3}
+      { name : "work", runs : []},
+      { name : "sleep", runs : []},
+      { name : "play", runs : []}
     ];
-
-    // Global clock status
-    this.running = false;
-    this.selectedCategory = null;
-    this.simulation = null;
-
-    // Bind exposed functions
-    this.bubbleSize = this.bubbleSize.bind(this);
-    this.updateSizes = this.updateSizes.bind(this);
-    this.resetSimulation = this.resetSimulation.bind(this);
-    this.handleTick = this.handleTick.bind(this);
-    this.getTotalTime = this.getTotalTime.bind(this);
 
     window.addCategory = (category) => {
       this.categories.push({name: category, runs: [], color: 2});
-      console.log(this.categories);
+      this.updateDisplay();
     }
+
+    this.getTotalTime = this.getTotalTime.bind(this);
+    this.bubbleSize = this.bubbleSize.bind(this);
+    this.updateSizes = this.updateSizes.bind(this);
+    this.updateDisplay = this.updateDisplay.bind(this);
+    this.loop = this.loop.bind(this);
  }
 
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+getTotalTime(runs) {
+  var totalTime = 0;
+  runs.forEach(run => {
+    totalTime += ( run.end || Date.now() ) - run.start;
+  });
+  return totalTime;
+}
+
+// Calculate relative bubble size
+bubbleSize(bubble) {
+  var allTimes = this.categories.map(entry => this.getTotalTime(entry.runs));
+  var min = Math.min(...allTimes);
+  var max = Math.max(...allTimes);
+
+  var scale = d3.scaleLinear()
+    .domain([min, max])
+    .range([30, 100]);
+
+  var radius = scale(this.getTotalTime(bubble.runs));
+  bubble.radius = radius;
+  return radius;
+}
+
+// Update the sizes of bubbles
+updateSizes() {
+  d3.selectAll("circle")
+  .attr("r", d => this.bubbleSize(d));
+
+  this.simulation
+    .nodes(this.categories)
+    .alpha(0.3)
+    .restart();
+}
+
+updateDisplay() {
+
+  // SETUP: Draw some test bubbles
+  var bubbles = this.svg
+    .selectAll("g")
+    .data(this.categories);
+
+  // ENTER:
+  var bubbleGroup = bubbles.enter()
+    .append("g");
+
+  bubbleGroup
+    .append("circle")
+    .attr("r", d => this.bubbleSize(d))
+    .style("fill", d => d3.interpolateTurbo(Math.random()))
+    .attr("stroke", "white")
+    .style("stroke-width", 2)
+    .on("click", (e, d) => { 
+      e.stopPropagation();
+      var length = d.runs.length;
+      // at least one entry and no end date on most recent
+      var isRunning = length != 0 && !d.runs[length-1].end;
+      if (isRunning) {
+        d.runs[length-1].end = Date.now();
+      } else {
+        d.runs.push({start: Date.now()});
+      }
+    })
+
+  bubbleGroup
+    .append("text")
+    .text("0")
+    .attr("stroke", "white")
+    .attr("fill", "white")
+    .attr("lengthAdjust", "spacingAndGlyphs")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .on("click", (e, d) => { 
+      e.stopPropagation();
+      var length = d.runs.length;
+      // at least one entry and no end date on most recent
+      var isRunning = length != 0 && !d.runs[length-1].end;
+      if (isRunning) {
+        d.runs[length-1].end = Date.now();
+      } else {
+        d.runs.push({start: Date.now()});
+      }
+    })
+
+  // REMOVE: When a category is removed
+  bubbles.exit().remove();
+
+  // RESTART SIMULATION
+
+  this.simulation
+    .nodes(this.categories)
+    .alpha(0.3)
+    .restart();
+}
+
+  // Set up timer loop
+  async loop() {
+    while (true) {
+      this.updateSizes();
+      await this.sleep(10);
+    }
   }
 
-  getTotalTime(runs) {
-    var totalTime = 0;
-    runs.forEach(run => {
-      totalTime += ( run.end || Date.now() ) - run.start;
-    });
-    return totalTime;
-  }
-
-  // Calculate relative bubble size
-  bubbleSize(bubble) {
-    var allTimes = this.categories.map(entry => this.getTotalTime(entry.runs));
-    var min = Math.min(...allTimes);
-    var max = Math.max(...allTimes);
-
-    var scale = d3.scaleLinear()
-      .domain([min, max])
-      .range([30, 100]);
-
-    var radius = scale(this.getTotalTime(bubble.runs));
-    bubble.radius = radius;
-    return radius;
-  }
-
-  // Update the sizes of bubbles
-  updateSizes() {
-    d3.selectAll("circle")
-    .attr("r", d => this.bubbleSize(d));
-  }
-
-  // Reset the simulation
-  resetSimulation() {
-
-    // Update the radius of the individual circles
-    this.simulation
-    .force("collide", d3.forceCollide().strength(.5).radius((d) => {
-      var radius = this.bubbleSize(d) + 3;
-      return radius;
-    }).iterations(1));
-
-    // Restart the simulation
-    this.simulation.alpha(0.3).restart();
-  }
-
-  // Handle clicks
-  handleTick() {
-    // Reset the simulation
-    this.updateSizes();
-    this.resetSimulation();
-  }
-
- componentDidMount() {
+ async componentDidMount() {
 
   var baseDiv = this.drawDiv.current;
   var categories = this.categories;
@@ -96,102 +142,30 @@ class App extends React.Component {
   // SETUP: Create SVG for clock visualization
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var svg = d3.select(baseDiv)
+  this.svg = d3.select(baseDiv)
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  // A color scale
-  var color = d3.scaleOrdinal()
-    .domain([1, 2, 3])
-    .range([ "#F8766D", "#00BA38", "#619CFF"])
+    // Features of the forces applied to the nodes:
+    this.simulation = d3.forceSimulation()
+      .force("x", d3.forceX().x(width/2).strength(.1))
+      .force("y", d3.forceY().y(height/2).strength(.1))
+      .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+      .force("collide", d3.forceCollide().strength(.5).radius((d) => {
+        return this.bubbleSize(d) + 3;
+      }).iterations(1)); // Force that avoids circle overlapping
 
-  // SETUP: Draw some test bubbles
-  var bubbleGroups = svg.append("g")
-    .selectAll("circle")
-    .data(categories)
-    .enter()
-    .append("g");
-
-  // Initial bubbles
-  var bubbles = bubbleGroups
-      .append("circle")
-      .attr("class", "node")
-      .attr("r", d => this.bubbleSize(d, categories))
-      .attr("cx", width / 2)
-      .attr("cy", height / 2)
-      .style("fill", d => color(d.color))
-      .style("fill-opacity", 0.8)
-      .attr("stroke", "white")
-      .style("stroke-width", 3)
-      .on("click", (_event, d) => { 
-        var length = d.runs.length;
-        console.log(length);
-        // at least one entry and no end date on most recent
-        var isRunning = length != 0 && !d.runs[length-1].end;
-        if (isRunning) {
-          d.runs[length-1].end = Date.now();
-        } else {
-          d.runs.push({start: Date.now()});
-        }
-      });
-
-  // On bubble add (same thing)
-  bubbles.enter()
-      .append("circle")
-      .attr("class", "node")
-      .attr("class", "node")
-      .attr("r", d => this.bubbleSize(d, categories))
-      .attr("cx", width / 2)
-      .attr("cy", height / 2)
-      .style("fill", d => color(d.color))
-      .style("fill-opacity", 0.8)
-      .attr("stroke", "white")
-      .style("stroke-width", 3)
-      .on("click", (_event, d) => { 
-        var length = d.runs.length;
-        console.log(length);
-        // at least one entry and no end date on most recent
-        var isRunning = length != 0 && !d.runs[length-1].end;
-        if (isRunning) {
-          d.runs[length-1].end = Date.now();
-        } else {
-          d.runs.push({start: Date.now()});
-        }
-      });
-
-  
-  var bubbleLabels = bubbleGroups
-      .append("text")
-      .text("0")
-      .attr("stroke", "white")
-      .attr("fill", "white")
-      .attr("lengthAdjust", "spacingAndGlyphs")
-      .attr("x", width / 2)
-      .attr("y", height / 2)
-      .attr("text-anchor", "middle")
-      .attr("alignment-baseline", "central")
-
-  // Features of the forces applied to the nodes:
-  this.simulation = d3.forceSimulation()
-    .force("x", d3.forceX().x(width/2).strength(.1))
-    .force("y", d3.forceY().y(height/2).strength(.1))
-    .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-    .force("collide", d3.forceCollide().strength(.5).radius((d) => {
-      var radius = this.bubbleSize(d, categories) + 3;
-      return radius;
-    }).iterations(1)); // Force that avoids circle overlapping
-
-  // Run the force simulation
-  this.simulation
+    // Run the force simulation
+    var svg = this.svg;
+    this.simulation
     .nodes(categories)
     .on("tick", function(d){
-
-      bubbles
+      svg.selectAll("circle")
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
 
-      bubbleLabels
+      svg.selectAll("text")
         .attr("x", d => d.x)
         .attr("y", d => d.y)
         .text(d => {
@@ -203,15 +177,14 @@ class App extends React.Component {
         })
   });
 
-  // Set up timer loop
-  var loop = async () => {
-    while (true) {
-      this.handleTick();
-      await this.sleep(10);
-    }
- }
+  // Add new category
+  svg.on("click", () => {
+    categories.push({name: "test", runs: []});
+    this.updateDisplay();
+  });
 
- loop();
+  this.updateDisplay();
+  this.loop();
 }
 
  render() {
