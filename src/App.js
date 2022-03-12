d8c8d51ffe689ffa670991241f576e2c1b5191f2
maxwell <maxwell@butthole.town>
2022-03-12 10:09:24 -0800

    Add timer boxes and lines
    Lots of small tweaks to animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import { timeSaturday } from 'd3';
 
 class App extends React.Component {
 
@@ -7,7 +8,7 @@ class App extends React.Component {
     super(props);
 
     // Draw area
-    this.drawDiv = React.createRef();
+    this.rootDiv = React.createRef();
 
     // Global clock data
     this.categories = [
@@ -43,12 +44,15 @@ getTotalTime(runs) {
 // Calculate relative bubble size
 bubbleSize(bubble) {
   var allTimes = this.categories.map(entry => this.getTotalTime(entry.runs));
+  var MIN_RADIUS = 10;
+  var MAX_RADIUS = 150;
+  var MIN_TRANSITION_TIME = 10000;
   var min = Math.min(...allTimes);
-  var max = Math.max(...allTimes);
+  var max = Math.max(Math.max(...allTimes), MIN_TRANSITION_TIME);
 
   var scale = d3.scaleLinear()
     .domain([min, max])
-    .range([20, 200]);
+    .range([MIN_RADIUS, MAX_RADIUS]);
 
   var radius = scale(this.getTotalTime(bubble.runs));
   bubble.radius = radius;
@@ -57,7 +61,9 @@ bubbleSize(bubble) {
 
 // Update the sizes of bubbles
 updateSizes() {
-  d3.selectAll("circle")
+  d3
+  .selectAll("#bubbleGroup")
+  .selectAll("circle")
   .attr("r", d => this.bubbleSize(d));
 
   this.simulation
@@ -67,22 +73,24 @@ updateSizes() {
 }
 
 updateDisplay() {
+  var GAP_SIZE = 20;
+  var NAV_WIDTH = 200;
+  var STROKE_WIDTH = 3;
+  var TIMER_HEIGHT = 50;
+  var MAX_COLORS = 10;
+  var categories = this.categories;
 
-  // SETUP: Draw some test bubbles
-  var bubbles = this.svg
-    .selectAll("g")
+  var bubbles = d3
+    .selectAll("#bubbleGroup")
+    .selectAll("circle")
     .data(this.categories);
 
-  // ENTER:
-  var bubbleGroup = bubbles.enter()
-    .append("g");
-
-  bubbleGroup
+  bubbles.enter()
     .append("circle")
     .attr("r", d => this.bubbleSize(d))
-    .style("fill", d => d3.interpolateTurbo(Math.random()))
+    .style("fill", d => d3.interpolateTurbo(1 - (categories.indexOf(d) % MAX_COLORS) / MAX_COLORS))
     .attr("stroke", "white")
-    .style("stroke-width", 2)
+    .style("stroke-width", STROKE_WIDTH)
     .on("click", (e, d) => { 
       e.stopPropagation();
       var length = d.runs.length;
@@ -93,32 +101,108 @@ updateDisplay() {
       } else {
         d.runs.push({start: Date.now()});
       }
-    })
+    }).on("mouseover", (_event, d) => {
+      d3.selectAll("#lineGroup")
+        .selectAll("line")
+        .attr("stroke", d2 => {
+          console.log(d);
+          console.log(d2);
+          console.log(d == d2);
+          return d == d2 ? "white" : "none"
+        });
+    });
 
-  bubbleGroup
-    .append("text")
-    .text("0")
-    .attr("stroke", "white")
-    .attr("fill", "white")
-    .attr("lengthAdjust", "spacingAndGlyphs")
-    .attr("text-anchor", "middle")
-    .attr("alignment-baseline", "central")
-    .on("click", (e, d) => { 
-      e.stopPropagation();
-      var length = d.runs.length;
-      // at least one entry and no end date on most recent
-      var isRunning = length != 0 && !d.runs[length-1].end;
-      if (isRunning) {
-        d.runs[length-1].end = Date.now();
-      } else {
-        d.runs.push({start: Date.now()});
-      }
-    })
-
-  // REMOVE: When a category is removed
   bubbles.exit().remove();
 
-  // RESTART SIMULATION
+  var timers = d3
+    .selectAll("#timerGroup")
+    .selectAll("rect")
+    .data(this.categories)
+
+  timers.enter()
+    .append("rect")
+    .attr("x", GAP_SIZE)
+    .attr("y", d => {
+      var index = categories.indexOf(d);
+      return GAP_SIZE + index * TIMER_HEIGHT;
+    })
+    .attr("width", NAV_WIDTH)
+    .attr("height", TIMER_HEIGHT)
+    .attr("stroke", "white")
+    .style("stroke-width", STROKE_WIDTH)
+    .on("mouseover", (_event, d) => {
+      d3.selectAll("#lineGroup")
+        .selectAll("line")
+        .attr("stroke", d2 => {
+          console.log(d);
+          console.log(d2);
+          console.log(d == d2);
+          return d == d2 ? "white" : "none"
+        });
+    }).on("click", (e, d) => { 
+      e.stopPropagation();
+      var length = d.runs.length;
+      // at least one entry and no end date on most recent
+      var isRunning = length != 0 && !d.runs[length-1].end;
+      if (isRunning) {
+        d.runs[length-1].end = Date.now();
+      } else {
+        d.runs.push({start: Date.now()});
+      }
+    })
+
+  timers.enter()
+    .append("circle")
+    .attr("r", TIMER_HEIGHT / 4)
+    .attr("cx", GAP_SIZE + TIMER_HEIGHT / 2)
+    .attr("cy", d => {
+      var index = categories.indexOf(d);
+      return GAP_SIZE + TIMER_HEIGHT / 2 + index * TIMER_HEIGHT;
+    })
+    .style("fill", d => d3.interpolateTurbo(1 - (categories.indexOf(d) % MAX_COLORS) / MAX_COLORS))
+    .attr("stroke", "white")
+    .style("stroke-width", STROKE_WIDTH)
+    .on("mouseover", (_event, d) => {
+      d3.selectAll("#lineGroup")
+        .selectAll("line")
+        .attr("stroke", d2 => {
+          console.log(d);
+          console.log(d2);
+          console.log(d == d2);
+          return d == d2 ? "white" : "none"
+        });
+    }).on("click", (e, d) => { 
+      e.stopPropagation();
+      var length = d.runs.length;
+      // at least one entry and no end date on most recent
+      var isRunning = length != 0 && !d.runs[length-1].end;
+      if (isRunning) {
+        d.runs[length-1].end = Date.now();
+      } else {
+        d.runs.push({start: Date.now()});
+      }
+    })
+
+  timers.exit().remove();
+
+  var lines = d3
+    .selectAll("#lineGroup")
+    .selectAll("line")
+    .data(categories);
+
+  lines.enter()
+    .append("line")
+    .attr("x1", GAP_SIZE + NAV_WIDTH)
+    .attr("y1", d => {
+      var index = categories.indexOf(d);
+      return GAP_SIZE + TIMER_HEIGHT * index + TIMER_HEIGHT / 2;
+    })
+    .attr("x2", window.innerWidth / 2)
+    .attr("y2", window.innerHeight / 2)
+    .attr("stroke", "none")
+    .style("stroke-width", STROKE_WIDTH)
+
+  lines.exit().remove();
 
   this.simulation
     .nodes(this.categories)
@@ -136,45 +220,65 @@ updateDisplay() {
 
  async componentDidMount() {
 
-  var baseDiv = this.drawDiv.current;
+  var rootDiv = this.rootDiv.current;
   var categories = this.categories;
+  var viewWidth = window.innerWidth;
+  var viewHeight = window.innerHeight;
+  var GAP_SIZE = 20;
+  var NAV_WIDTH = 200;
+  var STROKE_WIDTH = 3;
 
-  // SETUP: Create SVG for clock visualization
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  this.svg = d3.select(baseDiv)
+  // Create root svg element for drawing
+  var svg = d3.select(rootDiv)
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", viewWidth)
+    .attr("height", viewHeight)
 
-    // Features of the forces applied to the nodes:
-    this.simulation = d3.forceSimulation()
-      .force("x", d3.forceX().x(width/2).strength(.1))
-      .force("y", d3.forceY().y(height/2).strength(.1))
-      .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-      .force("collide", d3.forceCollide().strength(1).radius((d) => {
-        return this.bubbleSize(d) + 3;
-      }).iterations(10)); // Force that avoids circle overlapping
+  // Create visual bounding box for bubbles
+  var simBox = svg
+    .append("rect")
+    .attr("x", 2 * GAP_SIZE + NAV_WIDTH) //
+    .attr("y", GAP_SIZE)
+    .attr("width", viewWidth - NAV_WIDTH - 3 * GAP_SIZE)
+    .attr("height", viewHeight - 2 * GAP_SIZE)
+    .attr("stroke", "white")
+    .style("stroke-width", STROKE_WIDTH);
 
-    // Run the force simulation
-    var svg = this.svg;
-    this.simulation
-    .nodes(categories)
-    .on("tick", function(d){
-      svg.selectAll("circle")
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y)
+  // Create group for bubble vis
+  svg.append("g")
+    .attr("id", "bubbleGroup");
 
-      svg.selectAll("text")
-        .attr("x", d => d.x)
-        .attr("y", d => d.y)
-        .text(d => {
-          var totalTime = 0;
-          d.runs.forEach(run => {
-            totalTime += ( run.end || Date.now() ) - run.start;
-          });
-          return totalTime;
-        })
+  // Create group for box timers
+  svg.append("g")
+    .attr("id", "timerGroup");
+
+  // Create group for connecting lines
+  svg.append("g")
+    .attr("id", "lineGroup");
+
+  // Features of the forces applied to the nodes:
+  this.simulation = d3.forceSimulation()
+    .force("x", d3.forceX().x((viewWidth + GAP_SIZE + NAV_WIDTH)/2).strength(.1)) // X component of gravity
+    .force("y", d3.forceY().y(viewHeight/2).strength(.2)) // Y component of gravity
+    .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+    .force("collide", d3.forceCollide().strength(1).radius((d) => {
+      return this.bubbleSize(d) + 3;
+    }).iterations(100)); // Force that avoids circle overlapping
+
+  // Run the force simulation
+  this.simulation
+  .nodes(categories)
+  .on("tick", () => {
+
+    d3.selectAll("#bubbleGroup")
+      .selectAll("circle")
+      .attr("cx", d => Math.max(270 + d.radius, d.x))
+      .attr("cy", d => Math.max(20 + d.radius, d.y))
+
+    d3.selectAll("#lineGroup")
+      .selectAll("line")
+      .attr("x2", d => Math.max(270 + d.radius, d.x))
+      .attr("y2", d => Math.max(20 + d.radius, d.y))
   });
 
   // Add new category
@@ -189,7 +293,7 @@ updateDisplay() {
 
  render() {
   return [
-    <div ref={this.drawDiv}></div>
+    <div ref={this.rootDiv}></div>
   ];
  }
 }
